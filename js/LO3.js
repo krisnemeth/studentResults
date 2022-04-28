1f4375e52827c2867b476efca807fea1e1460f46
Krisztian Nemeth <krsnmth@gmail.com>
2022-04-28 11:11:44 +0100

    add and clear functions finished

--- js/LO3.js
+++ js/LO3.js
@@ -6,18 +6,50 @@ let studentMarks = [];
 
 // setting up Add button
 document.getElementById('Add').addEventListener('click', function(){
-    let name = document.getElementById('name').value;
-    let mark = document.getElementById('mark').value;
+    let name = document.getElementById('name').value.trim().toLowerCase();
+    let mark = Number(document.getElementById('mark').value);
 
-    if(studentNames.includes(name)){
-        document.getElementById('result').innerHTML = `This student's name has already been recorded!`;
+    if (!name) {
+        document.getElementById('result').innerHTML = `Please enter a name to go along with the mark.`;
+    } else if(!mark) {
+        document.getElementById('result').innerHTML = `Please enter a mark to go along with the name.`;
+    } else if (studentNames.includes(name)) {
+        document.getElementById('result').innerHTML = `This student's performance has already been recorded!`;
     } else {
         studentNames.push(name);
         studentMarks.push(mark);
 
-        document.getElementById('result').innerHTML = ` ${name}'s mark has been recorded!`
+        document.getElementById('name').value = '';
+        document.getElementById('mark').value = '';
+
+        document.getElementById('result').innerHTML = ` ${name}'s performance has been recorded!`
+
         console.log(studentNames);
         console.log(studentMarks);
     }
 })
 
+// setting up display button
+document.getElementById('Display').addEventListener('click', function(){
+    
+
+
+
+
+
+
+})
+
+// setting up clear button
+document.getElementById('Clear').addEventListener('click', function(){
+    studentNames = [];
+    studentMarks = [];
+
+    document.getElementById('name').value = '';
+    document.getElementById('mark').value = '';
+
+    document.getElementById('result').innerHTML = `Student records have been cleared.`;
+
+    console.log(studentNames);
+    console.log(studentMarks);
+})
