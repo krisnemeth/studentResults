56cdc72ea3173e3b8c595eef0d8023518ed14c56
Krisztian Nemeth <krsnmth@gmail.com>
2022-04-29 16:12:54 +0100

    finished assessment

--- js/LO3.js
+++ js/LO3.js
@@ -1,36 +1,36 @@
 // setting up arrays for storing studentNames and studentGrades
-
 let studentNames = [];
-
 let studentMarks = [];
-
-
 
 // setting up Add button
 document.getElementById('Add').addEventListener('click', function(){
+    // getting input data of names and marks, converting marks to a number straight away
     let name = document.getElementById('name').value;
     let mark = Number(document.getElementById('mark').value);
 
+    // checking for conditions
     if (!name) {
-        document.getElementById('result').innerHTML = `Please enter a name to go along with the mark.`;
+        // if there's no name data, error msg is displayed
+        document.getElementById('result').innerHTML = `Please enter a name.`;
     } else if(!mark) {
-        document.getElementById('result').innerHTML = `Please enter a mark to go along with the name.`;
+        // if there's no mark data, error msg is displayed
+        document.getElementById('result').innerHTML = `Please enter a mark.`;
     } else if (studentNames.includes(name)) {
+        // if the user tries to enter the same student's name twice, error msg is displayed
         document.getElementById('result').innerHTML = `This student's performance has already been recorded!`;
+        // clearing the input
+        document.getElementById('name').value = '';
+        document.getElementById('mark').value = '';
     } else {
+        // if all conditions are met, pushing data to arrays
         studentNames.push(name);
         studentMarks.push(mark);
-
-       
-
+        // displaying a feedback msg to the user
         document.getElementById('result').innerHTML = ` ${name}'s performance has been recorded!`;
-
-        
-
-        console.log(studentNames);
-        console.log(studentMarks);
+        // clearing the input
+        document.getElementById('name').value = '';
+        document.getElementById('mark').value = '';
     }
-
     
 })
 
@@ -42,11 +42,11 @@ document.getElementById('Display').addEventListener('click', function(){
     // checking if there's data in the array at all
     if (studentNames.length === 0) {
         // displaying the error message in case there is no data
-        document.getElementById('result').innerHTML = `No results to display.`
+        document.getElementById('result').innerHTML = `No results to display.`;
     } else {
         // if there is data, looping through it and writing it out to the DOM
         for (i=0; i<studentNames.length; i++) {
-            document.getElementById('result').innerHTML += `Student: ${studentNames[i]} - ${studentMarks[i]} <br>`
+            document.getElementById('result').innerHTML += `Student: ${studentNames[i]} - Mark: ${studentMarks[i]} <br>`;
         }
     }
     
@@ -73,14 +73,14 @@ function calcAverage(numbers){
     // looping through the array
     for (i=0; i<numbers.length; i++) {
         // updating the total variable's value at every iteration
-        total += numbers[i];
+        total += (numbers[i] / numbers.length);
     }
 
     // returning the value of total
     return total;
 }
-// event listener attached to a button click using an anonymus function
+// event listener attached to a button click using an anonymous function
 document.getElementById('Average').addEventListener('click', function(){
     // writing out the total into the DOM passing the array of numbers as parameter
-    document.getElementById('result').innerHTML = `${calcAverage(studentMarks)}`
+    document.getElementById('result').innerHTML = `${calcAverage(studentMarks)}`;
 });
